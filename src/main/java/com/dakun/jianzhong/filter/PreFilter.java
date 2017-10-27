6658ad31a300797ba46fee919674acc80081535c
“lichenghai <haicheng5@126.com>
2017-10-28 01:42:22 +0800

    管理后台登录

--- src/main/java/com/dakun/jianzhong/filter/PreFilter.java
+++ src/main/java/com/dakun/jianzhong/filter/PreFilter.java
@@ -33,6 +33,7 @@ public class PreFilter extends ZuulFilter {
     private RestTemplate restTemplate;
 
     private RedisTemplate redisTemplate;
+
     @Autowired(required = false)
     public void setRedisTemplate(RedisTemplate redisTemplate) {
         RedisSerializer stringSerializer = new StringRedisSerializer();
@@ -45,6 +46,7 @@ public class PreFilter extends ZuulFilter {
 
     public PreFilter() {
     }
+
     @Override
     public String filterType() {
 
@@ -71,7 +73,7 @@ public class PreFilter extends ZuulFilter {
             String uri = request.getRequestURI();
 
             //*************************************处理图片
-            if(uri.startsWith("/image/")) {
+            if (uri.startsWith("/image/")) {
                 try {
                     String fileName = uri.split("/image/")[1];
                     if (fileName == null) {
@@ -83,23 +85,23 @@ public class PreFilter extends ZuulFilter {
                     }
                     Enumeration<String> enumerator = request.getParameterNames();
                     StringBuilder builder = new StringBuilder("");
-                    while(enumerator.hasMoreElements()){
+                    while (enumerator.hasMoreElements()) {
                         builder.append(enumerator.nextElement());
                     }
                     String imageType = builder.toString();
-                    String downloadURL = QiniuFile.getdownloadurl(QiniuConstant.Domain_resources,fileName,imageType,QiniuConstant.portrait_download_app_exp);
-                    Map<String,List<String>> qp = new HashMap<String, List<String>>();
+                    String downloadURL = QiniuFile.getdownloadurl(QiniuConstant.Domain_resources, fileName, imageType, QiniuConstant.portrait_download_app_exp);
+                    Map<String, List<String>> qp = new HashMap<String, List<String>>();
                     String paramsPairs = downloadURL.split("\\?")[1];
                     String[] params = paramsPairs.split("&");
 
-                    for(int i = 0; i < params.length; i++) {
+                    for (int i = 0; i < params.length; i++) {
                         List<String> paramList = new ArrayList<>();
-                        paramList.add(params[i+1]);
-                        qp.put(params[i],paramList);
+                        paramList.add(params[i + 1]);
+                        qp.put(params[i], paramList);
                         i++;
                     }
                     ctx.setRequestQueryParams(qp);
-                } catch(Exception e) {
+                } catch (Exception e) {
                     Map<String, Object> result = new HashMap<String, Object>();
                     result.put("message", "图片地址错误");
                     result.put("status", 404);
@@ -112,33 +114,35 @@ public class PreFilter extends ZuulFilter {
 
             //*************************************登陆验证码
             if (uri.equals("/account-service/user/login")) {
-                Map<String,List<String>> requestParams = ctx.getRequestQueryParams();
+                Map<String, List<String>> requestParams = ctx.getRequestQueryParams();
                 String code = requestParams.get("code").get(0);
                 String phone = requestParams.get("mobile").get(0);
                 ValueOperations<String, Object> operations = redisTemplate.opsForValue();
                 String redisKey = TextUtils.getSMSRedisKey(phone);
-                String storedCode = (String)operations.get(TextUtils.getSMSRedisKey(phone));
-                if(storedCode == null) {
+                String storedCode = (String) operations.get(TextUtils.getSMSRedisKey(phone));
+                if (storedCode == null) {
                     ctx.setResponseStatusCode(401);
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    result.put("message","验证码已过期,请重新获取！");
-                    result.put("status",401);
+                    Map<String, Object> result = new HashMap<String, Object>();
+                    result.put("message", "验证码已过期,请重新获取！");
+                    result.put("status", 401);
                     ctx.setSendZuulResponse(false);
                     ctx.setResponseBody(JSON.toJSONString(result));
-                    ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
+                    ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
                     return null;
                 }
-                if(!code.equals(storedCode)) {
+                if (!code.equals(storedCode)) {
                     ctx.setResponseStatusCode(401);
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    result.put("message","验证码错误！");
-                    result.put("status",401);
+                    Map<String, Object> result = new HashMap<String, Object>();
+                    result.put("message", "验证码错误！");
+                    result.put("status", 401);
                     ctx.setSendZuulResponse(false);
                     ctx.setResponseBody(JSON.toJSONString(result));
-                    ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
+                    ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
                     return null;
                 }
                 redisTemplate.delete(redisKey);
+                return null;
+            } else if (uri.equals("/account-service/admin/login")) {
                 return null;
             }
             //*************************************处理权限
@@ -154,70 +158,74 @@ public class PreFilter extends ZuulFilter {
                     entity,
                     Map.class);
 
-            Map<String,Object> res = response.getBody();
-            List<Map<String,Object>> data = (List<Map<String,Object>>)res.get("data");
+            Map<String, Object> res = response.getBody();
+            List<Map<String, Object>> data = (List<Map<String, Object>>) res.get("data");
 
             //API 不存在
-            if(data.size() < 1) {
+            if (data.size() < 1) {
                 ctx.setSendZuulResponse(false);
                 ctx.setResponseStatusCode(404);
-                Map<String,Object> result = new HashMap<String,Object>();
-                result.put("message","未找到该资源api");
-                result.put("status",404);
+                Map<String, Object> result = new HashMap<String, Object>();
+                result.put("message", "未找到该资源api");
+                result.put("status", 404);
                 ctx.setResponseBody(JSON.toJSONString(result));
-                ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
-            }
-            else {
+                ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
+            } else {
                 //API 存在，而且不公开
-                if(!(Boolean)data.get(0).get("isPublic")) {
+                if (!(Boolean) data.get(0).get("isPublic")) {
                     String jwt = request.getHeader("Access-Token");
                     //没有jwt
-                    if(jwt == null) {
+                    if (jwt == null) {
                         ctx.setSendZuulResponse(false);
                         ctx.setResponseStatusCode(401);
-                        Map<String,Object> result = new HashMap<String,Object>();
-                        result.put("message","用户未登陆");
-                        result.put("status",401);
+                        Map<String, Object> result = new HashMap<String, Object>();
+                        result.put("message", "用户未登陆");
+                        result.put("status", 401);
                         ctx.setResponseBody(JSON.toJSONString(result));
-                        ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
-                    }
-                    else {
+                        ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
+                    } else {
                         try {
                             String deviceIdParam = request.getHeader("deviceId");
-                            Claims claim = JWTUtils.parseJWT(jwt);
-                            System.out.println("accountId:"+claim.getId());
-                            System.out.println("subject:"+claim.getSubject());
-                            ctx.addZuulRequestHeader("accountId",claim.getId());
-                            JSONObject object = JSONObject.parseObject(claim.getSubject());
-                            ctx.addZuulRequestHeader("usertype",object.getString("role"));
-                            String deviceId = object.getString("deviceId");
-                            if(!deviceId.equals(deviceIdParam)) {
-                                ctx.setSendZuulResponse(false);
-                                ctx.setResponseStatusCode(402);
-                                Map<String,Object> result = new HashMap<String,Object>();
-                                result.put("message","登陆异常");
-                                result.put("status",402);
-                                ctx.setResponseBody(JSON.toJSONString(result));
-                                ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
+                            //如果是网站来源
+                            if (!"websource".equals(deviceIdParam)) {
+                                Claims claim = JWTUtils.parseJWT(jwt);
+                                ctx.addZuulRequestHeader("adminId", claim.getId());
+                                JSONObject object = JSONObject.parseObject(claim.getSubject());
+                                ctx.addZuulRequestHeader("usertype", object.getString("role"));
+                            }else {
+                                Claims claim = JWTUtils.parseJWT(jwt);
+                                System.out.println("accountId:" + claim.getId());
+                                System.out.println("subject:" + claim.getSubject());
+                                ctx.addZuulRequestHeader("accountId", claim.getId());
+                                JSONObject object = JSONObject.parseObject(claim.getSubject());
+                                ctx.addZuulRequestHeader("usertype", object.getString("role"));
+                                String deviceId = object.getString("deviceId");
+                                if (!deviceId.equals(deviceIdParam)) {
+                                    ctx.setSendZuulResponse(false);
+                                    ctx.setResponseStatusCode(402);
+                                    Map<String, Object> result = new HashMap<String, Object>();
+                                    result.put("message", "登陆异常");
+                                    result.put("status", 402);
+                                    ctx.setResponseBody(JSON.toJSONString(result));
+                                    ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
+                                }
                             }
                             //读取claim, 增加权限控制
-                        }
-                        catch (Exception e){
+                        } catch (Exception e) {
                             //有jwt,但是无法正常解析，
                             ctx.setSendZuulResponse(false);
                             ctx.setResponseStatusCode(401);
-                            Map<String,Object> result = new HashMap<String,Object>();
-                            result.put("message","登陆异常或登陆状态已过期");
-                            result.put("status",401);
+                            Map<String, Object> result = new HashMap<String, Object>();
+                            result.put("message", "登陆异常或登陆状态已过期");
+                            result.put("status", 401);
                             ctx.setResponseBody(JSON.toJSONString(result));
-                            ctx.addZuulResponseHeader("Content-Type","application/json;charset=UTF-8");
+                            ctx.addZuulResponseHeader("Content-Type", "application/json;charset=UTF-8");
                         }
                     }
                 }
             }
             return null;
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(500);
             return null;
