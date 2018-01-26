2f09711496b08b65b0bcabe9b4b157bb48706516
wangjie <wangjie@fuhua.com>
2018-01-26 19:57:46 +0800

    zuul 错误过滤器

---
+++ src/main/java/com/dakun/jianzhong/filter/error/ErrorFilter.java
@@ -0,0 +1,42 @@
+package com.dakun.jianzhong.filter.error;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author wangjie
+ * @date 1/26/2018
+ */
+public class ErrorFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(ErrorFilter.class);
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        Throwable throwable = ctx.getThrowable();
+        logger.error("ErrorFilter message: {}", throwable.getCause().getMessage());
+        ctx.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        ctx.set("error.exception", throwable.getCause());
+        return null;
+    }
+}
