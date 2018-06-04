627f077f9324339110fa4cf80ae2b1dc8dcc2548
“lliuqiulan <717622427@qq.com>
2018-06-04 18:15:48 +0800

    针对业务方法返回list，list对象包含JSONObject,object对象包含list的处理

--- src/main/java/com/dakun/jianzhong/filter/PostFilter.java
+++ src/main/java/com/dakun/jianzhong/filter/PostFilter.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.dakun.jianzhong.service.qiniu.QiniuConstant;
 import com.dakun.jianzhong.service.qiniu.QiniuFile;
+import com.dakun.jianzhong.utils.BeanUtils;
 import com.dakun.jianzhong.utils.JWTUtils;
 import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
@@ -20,6 +21,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -125,98 +129,49 @@ public class PostFilter extends ZuulFilter {
                 String data = responsepic.get("data").toString();
                 System.out.println("data:" + data);
 
-                Boolean is = false;
-                for (String str : QiniuConstant.pictureMap.values()) {
-                    if (data.contains(str)) {
-                        is = true;
-                        break;
-                    }
-                }
-                if (is) {
-                    //对图片进行处理：结果可能是数组也可能是对象
-                    //整个结果进行匹配，遍历JSONObject对象，根据文件名获取key，更改value。
-                    //整个结果进行匹配，遍历JSONArray数组，将结果转换为JSONObject，根据文件名获取key，更改value。
-                    if (data.startsWith("[")) {
-                        JSONArray array = JSONArray.parseArray(data);
-                        //array数组包含多个jsonObject对象,遍历多个对象
-                        for (int i = 0; i < array.size() - 1; i++) {
-                            JSONObject object = JSON.parseObject(array.get(i).toString());
-                            for (String str : QiniuConstant.pictureMap.values()) {//遍历map值
-                                for (String s : object.keySet()) {//遍历返回结果值
-                                    Object value = object.get(s);
-                                    if (value.toString().contains(str)) {//获取对应的key与value
-                                        //logger.info("key1:"+s+"value1:"+object.get(s));
-                                        //获取到了对应的图片路径value
-                                        //根据deviceid做不同处理
-                                        String deviceId = request.getHeader("deviceId");
-                                        if ("websource".equals(deviceId) || "0000000062728c586110c7f90033c587".equals(deviceId)) {
-                                            //value可能是一个图片数组
-                                            StringBuilder stringBuilder = new StringBuilder();
-                                            String[] imageFileNames = value.toString().split(";");
-                                            for (String image : imageFileNames) {
-                                                stringBuilder.append(QiniuFile.getPrivateDownloadUrl(image)).append(";");
-                                            }
-                                            //图片后台做处理
-                                            //统一返回 "http://" + domain + "/" + key 格式
-                                            object.put(s, stringBuilder);
-                                        } else {
-                                            //app自行处理
-                                        }
-                                        //logger.info("key2:"+s+"value2:"+object.get(s));
-                                    }
-                                }
-                            }
-                            array.set(i, object);
+                String deviceId = request.getHeader("deviceId");
+                if ("websource".equals(deviceId) || "0000000062728c586110c7f90033c587".equals(deviceId)) {
+                    Boolean is = false;
+                    for (String str : QiniuConstant.pictureMap.values()) {
+                        if (data.contains(str)) {
+                            is = true;
+                            break;
                         }
-                        //System.out.println("arry:"+array.toString());
-                        responsepic.put("data", array);
-                        //ctx.setResponseBody(JSON.toJSONString(responsepic));
-                        InputStream rs = new ByteArrayInputStream(JSON.toJSONString(responsepic).getBytes());
-                        ctx.setResponseDataStream(rs);
-                        System.out.println("data1:" + array.toString());
-                    } else {
-                        JSONObject object = JSONObject.parseObject(data);
-                        for (String str : QiniuConstant.pictureMap.values()) {//遍历map值
-                            for (String s : object.keySet()) {//遍历返回结果值
-                                Object value = object.get(s);
-                                if (value.toString().contains(str)) {//获取对应的key与value
-                                    //logger.info("key1:"+s+"value1:"+object.get(s));
-                                    //获取到了对应的图片路径value
-                                    //根据deviceid做不同处理
-                                    String deviceId = request.getHeader("deviceId");
-                                    if ("websource".equals(deviceId) || "0000000062728c586110c7f90033c587".equals(deviceId)) {
-                                        //value可能是一个图片数组
-                                        StringBuilder stringBuilder = new StringBuilder();
-                                        String[] imageFileNames = value.toString().split(";");
-                                        for (String image : imageFileNames) {
-                                            stringBuilder.append(QiniuFile.getPrivateDownloadUrl(image)).append(";");
-                                        }
-                                        //图片后台做处理
-                                        //统一返回 "http://" + domain + "/" + key 格式
-                                        object.put(s, stringBuilder);
-                                    } else {
-                                        //app自行处理
-                                    }
-                                    //logger.info("key2:"+s+"value2:"+object.get(s));
-                                }
-                            }
-                        }
-                        responsepic.put("data", object);
-                        //ctx.setResponseBody(JSON.toJSONString(responsepic));
-                        InputStream rs = new ByteArrayInputStream(JSON.toJSONString(responsepic).getBytes());
-                        ctx.setResponseDataStream(rs);
-                        System.out.println("data1:" + object.toString());
                     }
-
+                    if (is) {
+                        //对图片进行处理：结果可能是数组也可能是对象
+                        //整个结果进行匹配，遍历JSONObject对象，根据文件名获取key，更改value。
+                        //整个结果进行匹配，遍历JSONArray数组，将结果转换为JSONObject，根据文件名获取key，更改value。
+                        if (data.startsWith("[")) {
+                            JSONArray array = JSONArray.parseArray(data);
+                            //array数组包含多个jsonObject对象,遍历多个对象
+                            for (int i = 0; i < array.size() - 1; i++) {
+                                JSONObject object = setImageUrl(array.get(i).toString());
+                                array.set(i, object);
+                            }
+                            //System.out.println("arry:"+array.toString());
+                            responsepic.put("data", array);
+                            //ctx.setResponseBody(JSON.toJSONString(responsepic));
+                            InputStream rs = new ByteArrayInputStream(JSON.toJSONString(responsepic).getBytes());
+                            ctx.setResponseDataStream(rs);
+                            System.out.println("data1:" + array.toString());
+                        } else {
+                            JSONObject object = setImageUrl(data);
+                            responsepic.put("data", object);
+                            //ctx.setResponseBody(JSON.toJSONString(responsepic));
+                            InputStream rs = new ByteArrayInputStream(JSON.toJSONString(responsepic).getBytes());
+                            ctx.setResponseDataStream(rs);
+                            System.out.println("data1:" + object.toString());
+                        }
+                    }
                 } else {
-                    //System.out.println(ctx.getResponseDataStream());
-                    // ctx.setResponseBody(JSON.toJSONString(responsepic));
                     // 所以重新创建流并通过ctx对象传递流
                     InputStream rs = new ByteArrayInputStream(JSON.toJSONString(responsepic).getBytes());
                     ctx.setResponseDataStream(rs);
                 }
+                //app自行处理
             } catch (IOException e) {
-                JSON jb = JSON.parseObject("{\"status\":210,\"message\":\"签名失败!\"}");
+                JSON jb = JSON.parseObject("{\"status\":210,\"message\":\"业务方法获取数据失败!\"}");
                 ctx.setResponseBody(jb.toString());
                 e.printStackTrace();
             }
@@ -226,6 +181,75 @@ public class PostFilter extends ZuulFilter {
             ctx.setResponseBody(jb.toString());
             e.printStackTrace();
             return null;
+        }
+    }
+
+    //对value遍历处理
+    public JSONObject setImageUrl(String data) {
+        JSONObject object = JSONObject.parseObject(data);
+        for (String str : QiniuConstant.pictureMap.values()) {//遍历map值
+            for (String s : object.keySet()) {//遍历返回结果值
+                Object value = object.get(s);
+                if (value.toString().contains(str)) {//获取对应的key与value
+                    if (value.toString().contains("{")) {
+                        //value是其他对象
+                        object.put(s, buildObj(value, str));
+                    } else {
+                        //获取到了对应的图片路径value
+                        //value可能是一个图片数组
+                        //图片后台做处理
+                        //统一返回 "http://" + domain + "/" + key 格式
+                        object.put(s, ImageToUrl(value.toString()));
+                    }
+                }
+            }
+        }
+        return object;
+    }
+
+    //包装图片地址
+    public String ImageToUrl(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] imageFileNames = str.split(";");
+        for (String image : imageFileNames) {
+            stringBuilder.append(QiniuFile.getPrivateDownloadUrl(image)).append(";");
+        }
+        return stringBuilder.toString();
+    }
+
+    //将object对象转换为map包装图片地址
+    public Map<String, Object> ObjectToMap(Map<String, Object> map, String str) {
+        //Map<String, Object> map = BeanUtils.beanProperties(obj);
+        //JSONObject map = JSONObject.parseObject(obj.toString());
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if(entry.getValue().toString().contains("[")){
+                buildObj(entry.getValue(),str);
+            }
+            if (entry.getValue().toString().contains(str)) {
+                resultMap.put(entry.getKey(), ImageToUrl(entry.getValue().toString()));
+                continue;
+            }
+            resultMap.put(entry.getKey(), entry.getValue());
+        }
+        return resultMap;
+    }
+
+    //对包含图片的数据进行包装
+    public JSON buildObj(Object object, String str) {
+        //Map<String, Object> map = BeanUtils.beanProperties(object);不能用BeanUtils方法
+        String s = object.toString();
+        if (s.startsWith("[")) {
+            JSONArray arry = JSONArray.parseArray(s);
+            for (int i = 0; i <= arry.size() - 1; i++) {
+                JSONObject arrayObj = JSON.parseObject(arry.get(i).toString());
+                arry.set(i, ObjectToMap(arrayObj, str));
+            }
+            return arry;
+        } else {
+            //对象
+            JSONObject object1 = JSON.parseObject(s);
+            return JSON.parseObject(ObjectToMap(object1, str).toString());
         }
     }
 }
